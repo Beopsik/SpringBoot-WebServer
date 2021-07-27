b1207a38b8c37626e4d331096c80e8a4142cdaa3
BeobSik <tjswl756@naver.com>
2021-07-27 14:35:00 +0900

    init

---
+++ src/test/java/me/whiteship/springbootwebserver/SpringBootWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.springbootwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
